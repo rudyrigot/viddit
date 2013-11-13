e0d4aedf78aed11b65b449babb77687ffa603895
delba <delba@server.fake>
2013-11-12 19:02:54 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viddit::Application.config.secret_key_base = 'eac77196493dff4d2b322e93ba351b0986d0122e107ac35e37ff476e9ee73cac80446192be055ac0e52c657cfcddfbc0128a35edb2e776f74ca23a2ff696bf9c'
