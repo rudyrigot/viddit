5940b21fd11295049248723686db4e03be0be4c6
delba <delba@server.fake>
2013-11-15 14:36:42 -0800

    videos#create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,15 @@ class VideosController < ApplicationController
   	@videos = Video.all
   	@video = Video.new
   end
+
+  def create
+    Video.create!(video_params)
+    redirect_to root_url
+  end
+
+private
+
+  def video_params
+    params.require(:video).permit(:video_url)
+  end
 end
